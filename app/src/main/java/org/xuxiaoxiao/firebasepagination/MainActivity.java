5bce387a239ab14ad85c5a10c8c0633113b58459
xiaoqianghou <2662651103@qq.com>
2017-04-20 18:52:06 +0800

    Handler().postDelayed(new Runnable()

--- app/src/main/java/org/xuxiaoxiao/firebasepagination/MainActivity.java
+++ app/src/main/java/org/xuxiaoxiao/firebasepagination/MainActivity.java
@@ -3,6 +3,7 @@ package org.xuxiaoxiao.firebasepagination;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -67,8 +68,15 @@ public class MainActivity extends AppCompatActivity {
             swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
                 @Override
                 public void onRefresh() {
-                    swipeRefreshLayout.setRefreshing(true);
-                    pageUp();
+                    Log.d("MainActivity_","onRefresh");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            swipeRefreshLayout.setRefreshing(false);
+                            pageUp();
+
+                        }
+                    },1200);
                 }
             });
             swipeRefreshLayout.setColorSchemeColors(
